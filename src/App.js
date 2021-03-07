4c99d7def3d67ffab0b587bf6f949d49f8f9d3f0
jesslau <jesslau.14@gmail.com>
2021-03-07 09:14:13 -0800

    added useState to buttons

--- src/App.js
+++ src/App.js
@@ -2,39 +2,61 @@ import './App.css';
 import firebase from './firebase.js';
 import Header from './components/Header.js';
 import foodLists from './components/foodLists';
+import { useState } from 'react';
 // import Tracker from './components/Tracker.js';
 // import AddButton from './components/AddButton.js'
 // import RemoveButton from './components/RemoveButton.js';
 
 
 function App() {
+  const [stock, setStock] = useState(0);
   return (
     <div className="App">
       <Header />
      <main>
 
       <section className="stock-main">
-        <h3>Our Current Stock</h3>
+          <h3>Our Current Stock</h3>
+          <div className="food-list">
+            <ul className="food-type">
+              {foodLists['stockItems'].map((stockItem) => {
+                const currentStock = stockItem.currentStock;
+                return (
+                  <>
+                  <div className="indiv-item">
+                  <li>{stockItem.type}</li>
+                  <li>{currentStock}/{stockItem.capacity}</li>
+                      <button onClick={() => setStock(stock + 1)}>+</button>
+                      <button onClick={() => setStock(stock - 1)}>-</button>
+
+                  </div>
+                   </>
+                )
+                })}
+           </ul>
+          </div>
+
+
+
+
+        {/* <h3>Our Current Stock</h3>
         <div className="food-list">
-        
-        <ul className="food-type">
-       {foodLists['stockItems'].map((stockItem)=> {
-          return(
-              <li>{stockItem.type}</li>
-          )
-        })}
-        </ul>
-        <ul className="stock-number">
-        {foodLists['stockItems'].map((stockItem) => {
-          return (
-                  <li>{stockItem.currentStock}/ {stockItem.capacity}</li>
-            )
-        })}
-
-        </ul>
-
-
-        </div>
+            <ul className="food-type">
+              {foodLists['stockItems'].map((stockItem) => {
+                return (
+                  <li>{stockItem.type}</li>
+                )
+              })}
+            </ul>
+            <ul className="stock-number">
+              {foodLists['stockItems'].map((stockItem) => {
+                const currentStock = stockItem.currentStock;
+                return (
+                  <li>{currentStock}/ {stockItem.capacity}</li>
+                )
+              })}
+            </ul>
+        </div> */}
        
 
       </section>
@@ -51,3 +73,5 @@ function App() {
 }
 
 export default App;
+
+//set up useState adn then pass setUseState as value to db.update()
--- src/components/foodLists.js
+++ src/components/foodLists.js
@@ -1,72 +1,73 @@
 import firebase from '../firebase';
-const dbRef = firebase.database().ref();
- const foodLists = {
-       stockItems : [
-        {
-        type: 'milk/ milk alternatives',
-        capacity: 5,
-        currentStock: ''
-        },
-
-        {
-        type:'protein',
-        capacity: 10,
-        currentStock: ''
-        },
-        {
-        type:'fruits',
-        capacity: 20,
-        currentStock: ''
-        },
-        {
-        type: 'veggies',
-        capacity: 20,
-        currentStock: ''
-        },
-        {
-        type: 'flour',
-        capacity: 5,
-        currentStock: ''
-        },
-        {
-        type: 'rice',
-        capacity: 5,
-        currentStock: ''
-        },
-        {
-        type: 'oils',
-        capacity: 5,
-        currentStock:''
-        },
-        {
-        type: 'bread',
-        capacity: 5,
-        currentStock:''
-        },
-        {
-        type: 'canned goods',
-        capacity: 10,
-        currentStock:''
-        },
-
-       ],
-
-       groceryItem : [
-        {
-        type: '',
-        amount: ''
-        },
-        {
-        type: '',
-        amount: ''
-        },
-        {
+    const dbRef = firebase.database().ref();
+     const foodLists = {
+           stockItems : [
+            {
+            type: 'milk/ milk alternatives',
+            capacity: 5,
+            currentStock: ''
+            },
+    
+            {
+            type:'protein',
+            capacity: 10,
+            currentStock: ''
+            },
+            {
+            type:'fruits',
+            capacity: 20,
+            currentStock: ''
+            },
+            {
+            type: 'veggies',
+            capacity: 20,
+            currentStock: ''
+            },
+            {
+            type: 'flour',
+            capacity: 5,
+            currentStock: ''
+            },
+            {
+            type: 'rice',
+            capacity: 5,
+            currentStock: ''
+            },
+            {
+            type: 'oils',
+            capacity: 5,
+            currentStock:''
+            },
+            {
+            type: 'bread',
+            capacity: 5,
+            currentStock:''
+            },
+            {
+            type: 'canned goods',
+            capacity: 10,
+            currentStock:''
+            },
+    
+           ],
+    
+           groceryItem : [
+            {
             type: '',
             amount: ''
-        },
-    
-       ]
-   };
+            },
+            {
+            type: '',
+            amount: ''
+            },
+            {
+                type: '',
+                amount: ''
+            },
+        
+           ]
+       };
+       
 
 //    dbRef.push(foodLists);
 export default foodLists;
